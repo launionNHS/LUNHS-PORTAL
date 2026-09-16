cd708f91c2089fb05c7182e9fb58bdad240a229b
launionnhs <lolito.deloso@deped.gov.ph>
2026-09-16 21:42:17 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="lunhs-v30-2-phone-portrait-v1";
+const CACHE="lunhs-v30-3-strict-portrait-v1";
 const SHELL=["./","./index.html","./styles.css","./lunhs-logo.png","./portal.html"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
