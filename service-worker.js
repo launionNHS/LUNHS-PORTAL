7a5a35a88011d6bf9b20cfdd3f0d67ccfe016494
launionnhs <lolito.deloso@deped.gov.ph>
2026-09-23 00:22:10 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,1 @@
-const CACHE="lunhs-v32-5-student-sex-grouping";
-const SHELL=["./","./index.html","./styles.css","./lunhs-logo.png","./portal.html","./calendar.html","./resources.html","./search.html"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
- if(e.request.method!=="GET")return;
- const u=new URL(e.request.url);
- if(u.hostname.includes("supabase.co"))return;
- e.respondWith(fetch(e.request).then(r=>{if(u.origin===location.origin){let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c))}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
-});
+const CACHE='lunhs-v33-school-year-management';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
